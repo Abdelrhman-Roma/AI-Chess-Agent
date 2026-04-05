6e21fe2bbf7350f2d8d0873fdc8a519741b0927c
Abdelrhman <abdelrhman.roma2006@gmail.com>
2026-04-05 13:28:39 +0200

    add full piece movement (rook ,knight , bishop ,queen , king) and improve move generation

--- src/game/rules.py
+++ src/game/rules.py
@@ -1,4 +1,5 @@
-from game.piece import pawn_moves
+# from game.piece import pawn_moves , rook_moves ,knight_moves , bishop_moves ,queen_moves
+from game.piece import *
 # بنستورد فانكشن حركات البيدق من ملف تاني
 
 def get_valid_moves(board,row,col):
@@ -21,7 +22,19 @@ def get_valid_moves(board,row,col):
         return pawn_moves(board ,row ,col)
         # ننادي فانكشن البيدق ونرجع الحركات بتاعته
     
-
+    #---------rook-----------------
+    elif piece in ["wr" ,"br"]:
+        return rook_moves(board,row,col)    
+    #---------knight--------------------
+    elif piece in ["wn" , "bn"]:
+        return knight_moves(board,row ,col)
+    #-------------bishop-------------------
+    elif piece in ["wb" ,"bb"]:
+        return bishop_moves(board,row ,col)
+    elif piece in ["wq" ,"bq"]:
+        return queen_moves(board,row,col)
+    elif piece in ["wk" ,"bk"]:
+        return king_moves(board,row,col)
     return []
     # باقي القطع (rook / knight / bishop / queen / king)
     # لسه مش متطبقة، فبيرجع ليستة فاضية
